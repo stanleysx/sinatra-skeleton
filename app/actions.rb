47c36e99a5a2b74ef1bd15e5a04d975c10aa9a1f
stanleysx <stanley.cho8@icloud.com>
2015-05-25 19:23:23 -0700

    May 25 V2

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,10 @@
 # Homepage (Root path)
+helpers do
+  def current_user
+    @current_user = User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+end
+
 get '/' do
   erb :index
 end
@@ -11,6 +17,41 @@ get '/signup' do
   erb :signup
 end
 
-get '/profile/edit' do
+get '/profile' do
   erb :profile
+end
+
+get '/movies/new' do
+  erb :new_movie
+end
+
+post '/login' do
+  username = params[:username]
+  password = params[:password]
+
+  user = User.find_by(username: username)
+  if user.password == password
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    redirect '/login'
+  end
+end
+
+post '/signup' do
+  username = params[:username]
+  password = params[:password]
+
+  user = User.find_by(username: username)
+  if user
+    redirect '/signup'
+  else
+    user = User.create(username: username, password: password)
+    session[:user_id] = user.id
+    redirect '/'
+  end
+end
+
+post '/profile' do
+  redirect '/'
 end
